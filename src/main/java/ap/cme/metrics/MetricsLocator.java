a2c95a754bbf2c2ed7affde6c86db8ca9435dea4
Alexey Prokopov <prokopov.alexey@gmail.com>
2022-02-21 09:55:01 +0300

    Seach metric services by interface

--- src/main/java/ap/cme/metrics/MetricsLocator.java
+++ src/main/java/ap/cme/metrics/MetricsLocator.java
@@ -1,35 +1,28 @@
 package ap.cme.metrics;
 
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @Scope("singleton")
 public class MetricsLocator {
+
     @Autowired
-    private ApplicationContext context;
-    
-    public IMetricsService getMetricsService(String version, String namespace, String service, String metricsName)
-    {
-        Map<String, Object> availableServices = context.getBeansWithAnnotation(MetricsService.class);
-        
-        for (Map.Entry<String, Object> pair: availableServices.entrySet()) {
-            if (pair.getValue() instanceof IMetricsService) {
-                IMetricsService metricsService = (IMetricsService) pair.getValue();
-                if (
-                    metricsService.getVersion().equals(version) && 
-                    metricsService.getNamespace().equals(namespace) && 
-                    metricsService.getServiceName().equals(service) && 
-                    metricsService.getName().equals(metricsName)
-                ) {
-                    return metricsService;
-                }
+    private List<IMetricsService> metricsServices;
+
+    public IMetricsService getMetricsService(String version, String namespace, String service, String metricsName) {
+        for (IMetricsService metricsService : metricsServices) {
+            if (metricsService.getVersion().equals(version)
+                && metricsService.getNamespace().equals(namespace)
+                && metricsService.getServiceName().equals(service)
+                && metricsService.getName().equals(metricsName)) {
+                return metricsService;
             }
         }
-        
+
         return null;
     }
 }
